ad48f982e3ceca7b9b5a5aee712d07c397b559f7
Merlyn <mciliasarellano@gmail.com>
2019-11-16 20:42:57 -0600

    Aplicacion de produccion lista

--- src/app/todo/todos-list/todos-list.component.ts
+++ src/app/todo/todos-list/todos-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/app.reducers';
 import { Todo } from '../model/todo.model';
+import { filtrosValidos } from 'src/app/filter/filter.actions';
+
 
 @Component({
   selector: 'app-todos-list',
@@ -11,7 +13,7 @@ import { Todo } from '../model/todo.model';
 export class TodosListComponent implements OnInit {
 
   todos: Todo[] = [];
-  filtro: string;
+  filtro: filtrosValidos;
 
   constructor(private store:Store<AppState>) { }
 
